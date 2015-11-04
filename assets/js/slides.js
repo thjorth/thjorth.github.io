2d5ef99d181d082687627915754d609b5f95e851
Thomas Hjorth <thomas.hjorth@valtech.com>
2015-11-04 20:10:10 +0100

    added some transitions and the reveal slide type.

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -4,14 +4,17 @@
 		next = document.querySelector("#next"),
 		dir = document.location.pathname.replace(/[^\/]+$/, ""),
 		siblingPages,
-		next;
+		next,
+		reveal = [];
 
 	document.addEventListener("keydown", function (e) {
 		console.log("keydown:");
 		console.log(e);
 		if (e.keyCode === 32) {
 			e.preventDefault();
-			if (next && next.url) {
+			if (reveal.length > 0) {
+				showNextHiddenElm();
+			} else if (next && next.url) {
 				document.location.pathname = next.url;
 			}
 		} else if (e.keyCode === 36) {
@@ -31,6 +34,21 @@
 		}
 		return previousValue;
 	}, undefined);
+
+	if (document.querySelector(".slide.reveal")) {
+		var domElms = document.querySelectorAll(".slide-content > *");
+		for (var i = 0; i < domElms.length; i++) {
+			reveal.push(domElms[i]);
+		}
+		setTimeout(function () {
+			showNextHiddenElm();
+		}, 250);
+	}
+
+	function showNextHiddenElm() {
+		var firstElm = reveal.shift();
+		firstElm.style.opacity = "1.0";
+	}
 
 	console.log(siblingPages);
 	console.log(next);
--- front-end-tooling/slide-00-the-modern-front-end-tool-stack.md
+++ front-end-tooling/slide-00-the-modern-front-end-tool-stack.md
@@ -1,5 +1,8 @@
 ---
 title: The modern front end tool stack
+type: reveal
 ---
 
-## This is a test
+### Or how I stopped worrying and love the bomb
+
+### eh, CSS, I mean
