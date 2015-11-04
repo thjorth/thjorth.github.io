f2d9d492a36c289b7c9329f5d14856c9dd3a43c5
Thomas Hjorth <thomas.hjorth@valtech.com>
2015-11-04 18:47:46 +0100

    Added dynamic paging

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,16 +1,37 @@
 (function () {
 
 	var 
-		next = document.querySelector("#next");
+		next = document.querySelector("#next"),
+		dir = document.location.pathname.replace(/[^\/]+$/, ""),
+		siblingPages,
+		next;
 
 	document.addEventListener("keydown", function (e) {
 		console.log("keydown:");
 		console.log(e);
 		if (e.keyCode === 32) {
 			e.preventDefault();
-			next.click();
+			if (next && next.url) {
+				document.location.pathname = next.url;
+			}
 		} else if (e.keyCode === 36) {
 			document.location.pathname = "/";
 		}
 	});
+
+	siblingPages = data.pages.filter(function (page) {
+		return page.url.indexOf(dir) === 0;
+	});
+	next = siblingPages.reduce(function (previousValue, currentValue) {
+
+		if (currentValue.url === document.location.pathname) {
+			return "found";
+		} else if (previousValue === "found") {
+			return currentValue;
+		}
+		return previousValue;
+	}, undefined);
+
+	console.log(siblingPages);
+	console.log(next);
 })();
