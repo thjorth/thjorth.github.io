1fc03b392b29734ad63be6051dd2718f1199ad7e
Thomas Hjorth <thomas.hjorth@valtech.com>
2015-11-08 22:53:22 +0100

    made the route animation time proportional to the length

--- assets/js/map.js
+++ assets/js/map.js
@@ -13,13 +13,14 @@
 			var path = paths.pop(),
 				length = path.getTotalLength();
 
+			console.log(length);
 			path.style.transition = path.style.WebkitTransition = "none";
 			path.style.strokeDasharray = length + " " + length;
 			path.style.strokeDashoffset = length;
 			path.getBoundingClientRect();
 
 			path.style.visibility = "visible";
-			path.style.transition = path.style.WebkitTransition = "stroke-dashoffset 2s ease-in-out";
+			path.style.transition = path.style.WebkitTransition = "stroke-dashoffset " + length / 15 + "s ease-in-out";
 			path.style.strokeDashoffset = "0";
 
 			setTimeout(showPath, 300);
